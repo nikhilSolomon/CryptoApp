a0c81936dff9af96a723968aba545ff6680dbcd5
Nikhil <nikhilsolomon0@gmail.com>
2023-05-02 12:02:29 +0530

    Initial Commit

---
+++ CryptoApp/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  CryptoApp
+//
+//  Created by nikhil-pt6881 on 02/05/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ CryptoApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CryptoApp
+//
+//  Created by nikhil-pt6881 on 02/05/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
